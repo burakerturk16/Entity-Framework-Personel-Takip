230af43133d413e3ae03d7161c7e8e88c0edcd0b
burakerturk16 <58392957+burakerturk16@users.noreply.github.com>
2021-02-01 01:44:26 +0300

    Add files via upload

---
+++ EfPersonelOtomasyonu/PersonelDal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace EfPersonelOtomasyonu
+{
+   public class PersonelDal
+    {
+        public List<Personeller> personelList()
+        {
+            using (EFPersonellerContext eFPersonellerContext = new EFPersonellerContext())
+            {
+                return eFPersonellerContext.Personeller.ToList();
+            }
+        }
+
+        public void personelEkle(Personeller P)
+        {
+            using (EFPersonellerContext eFPersonellerContext = new EFPersonellerContext())
+            {
+                eFPersonellerContext.Personeller.Add(P);
+                eFPersonellerContext.SaveChanges();
+            }
+        }
+        public void personelGuncelle(Personeller P)
+        {
+            using (EFPersonellerContext eFPersonellerContext = new EFPersonellerContext())
+            {
+                var guncelle = eFPersonellerContext.Entry(P);
+                guncelle.State = EntityState.Modified;
+                eFPersonellerContext.SaveChanges();
+            }
+        }
+
+        public void personelSil(Personeller P)
+        {
+            using (EFPersonellerContext eFPersonellerContext = new EFPersonellerContext())
+            {
+                var sil = eFPersonellerContext.Entry(P);
+                sil.State = EntityState.Deleted;
+                eFPersonellerContext.SaveChanges();
+            }
+        }
+    }
+}
---
+++ EfPersonelOtomasyonu/Personeller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfPersonelOtomasyonu
+{
+  public  class Personeller
+    {  
+        [Key]
+        public Guid PerID { get; set; }
+        public string PerAdi { get; set; }
+        public string PerSoyad { get; set; }
+        public string PerEmail { get; set; }
+        public string PerTelno { get; set; }
+    }
+}
+
